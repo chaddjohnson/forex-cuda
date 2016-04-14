81bffd1914afb9a4c1348cb14cd59d65421268a0
Chad Johnson <chad.d.johnson@gmail.com>
2016-04-14 01:04:44 -0500

    Fleshed things out further to get an idea of how long actual backtesting will take.

--- tests/class.cu
+++ tests/class.cu
@@ -4,18 +4,36 @@
 
 struct Strategy {
     double profitLoss;
-    void (*backtest)(struct Strategy *);
+    void (*backtest)(struct Strategy *, struct Tick *);
 };
 
-__device__ void backtest(struct Strategy *self) {
-    self->profitLoss++;
-}
+struct Tick {
+    long timestamp;
+    double open;
+    double high;
+    double low;
+    double close;
+    double rsi2;
+    double rsi5;
+    double rsi7;
+    double rsi9;
+    double rsi14;
+    double stochastic5K;
+    double stochastic5D;
+    double stochastic10K;
+    double stochastic10D;
+    double stochastic14K;
+    double stochastic14D;
+};
 
-__global__ void runBacktest(struct Strategy *strategies) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
+__device__ void backtest(struct Strategy *self, struct Tick *tick) {
+    int i;
+    int j = 0;
 
-    if (i < N) {
-        strategies[i].backtest(&strategies[i]);
+    // Pretend to do something.
+    // TODO: Actually do something useful.
+    for (i=0; i<50; i++) {
+        j++;
     }
 }
 
@@ -28,27 +46,57 @@ __global__ void initializeStrategies(struct Strategy *strategies) {
     }
 }
 
+__global__ void backtestStrategies(struct Strategy *strategies, struct Tick *tick) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < N) {
+        strategies[i].backtest(&strategies[i], tick);
+    }
+}
+
 int main() {
-    int threadsPerBlock = 10;
+    int threadsPerBlock = 1000;
     int blockCount = N / threadsPerBlock;
 
     struct Strategy strategies[N];
     struct Strategy *devStrategies;
     int i;
 
-    cudaMalloc((void**)&devStrategies, N * sizeof(Strategy));
-    cudaMemcpy(devStrategies, strategies, N * sizeof(Strategy), cudaMemcpyHostToDevice);
+    struct Tick tick = {1460611103, 89.5, 89.9, 89.2, 89.4, 89.7, 89.75, 89.72, 89.76, 89.9, 89.2, 89.4, 89.7, 89.75, 89.72, 89.76};
+    struct Tick *devTick;
 
+    // Allocate memory on the GPU for the strategies.
+    // TODO: Allocate memory on all GPUs.
+    cudaMalloc((void**)&devStrategies, N * sizeof(Strategy));
+    
+    // Initialize strategies on the GPU.
     initializeStrategies<<<blockCount, threadsPerBlock>>>(devStrategies);
-    runBacktest<<<blockCount, threadsPerBlock>>>(devStrategies);
+
+    for (i=0; i<1000000; i++) {
+        // Copy tick data to the GPU.
+        // TODO: Copy to all GPUs.
+        cudaMalloc((void**)&devTick, sizeof(Tick));
+        cudaMemcpy(devTick, &tick, sizeof(Tick), cudaMemcpyHostToDevice);
+
+        // Run backtests for all strategies.
+        // TODO: Run on all GPUs.
+        backtestStrategies<<<blockCount, threadsPerBlock>>>(devStrategies, devTick);
+
+        // Free memory for the tick from the GPU.
+        cudaFree(devTick);
+    }
 
     cudaDeviceSynchronize();
+
+    // Copy strategies from the GPU.
     cudaMemcpy(strategies, devStrategies, N * sizeof(Strategy), cudaMemcpyDeviceToHost);
 
+    // Display results.
     for (i=0; i<N; i++) {
         printf("%f\n", strategies[i].profitLoss);
     }
 
+    // Free memory for the strategies on the GPU.
     cudaFree(devStrategies);
 
     return 0;
